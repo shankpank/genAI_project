108c3eec5b3eb82ca4c2a3b1d6589212f2628819
shankpank <92246833+shankpank@users.noreply.github.com>
2024-04-30 15:35:03 +0530

    Update main.py

--- APP_11.py
+++ APP_11.py
@@ -1,85 +1,8 @@
-# import streamlit as st
-# from streamlit_chat import message
-# from langchain_core.output_parsers import StrOutputParser
-# import utils
- 
-# def initialize_session_state():
-#     """
-#     Session State is a way to share variables between reruns, for each user session.
-#     """
- 
-#     st.session_state.setdefault('history', [])
-#     st.session_state.setdefault('generated', ["Hello! I am here to provide answers to questions fetched from Database."])
-#     st.session_state.setdefault('past', ["Hello Buddy!"])
- 
-# def display_chat(chain):
-#     reply_container = st.container()
-#     container = st.container()
- 
-#     with container:
-#         with st.form(key='chat_form', clear_on_submit=True):
-#             user_input = st.text_input("Question:", placeholder="Ask me questions from MMS3", key='input')
-#             submit_button = st.form_submit_button(label='Send ⬆️')
-       
-#         #Check if user submit question with user input and generate response of the question
-#         if submit_button and user_input:
-#             generate_response(user_input,chain)
-   
-#     #Display generated response to streamlit web UI
-#     display_generated_responses(reply_container)
- 
-# def generate_response(user_input,chain):
-#     with st.spinner('Spinning a snazzy reply...'):
-#         output = conversation_chat(user_input,chain)
- 
-#     st.session_state['past'].append(user_input)
-#     st.session_state['generated'].append(output)
- 
-# def conversation_chat(user_input,chain):
-#     try:
-#         response = utils.search(user_input)
-#         print(response)
-#         final_response = chain.invoke(f"Based on the following information generate human readable response: (**Use only the below info to generate response): \
-#                                       Question: {user_input}\
-#                                       Answer: {response}")
-#         return final_response
-#     except Exception as e:
-#         error_statement = f"Sorry there is no relevant information about your question \'{user_input}\'.\
-#                             Try asking a differnt question or rephrase the question :("
-#         return error_statement
-   
- 
-# def display_generated_responses(reply_container):
-#     if st.session_state['generated']:
-#         with reply_container:
-#             for i in range(len(st.session_state['generated'])):
-#                 message(st.session_state["past"][i], is_user=True, key=f"{i}_user", avatar_style="adventurer")
-#                 message(st.session_state["generated"][i], key=str(i), avatar_style="bottts")
- 
-# def main():
-#     initialize_session_state()
-   
-#     st.title("MMS3 Chatbot🤖")
-   
-#     hide_streamlit_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             </style>
- 
-#             """
-#     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
-#     chain=utils.parser_output()
-#     display_chat(chain)
- 
-# if __name__ == "__main__":
-#     main()
-
 import streamlit as st
 from streamlit_chat import message
-from langchain_core.output_parsers import StrOutputParser
-import utils
+import utils3
+import pyperclip
+import pandas as pd
  
 def initialize_session_state():
     """
@@ -90,7 +13,7 @@ def initialize_session_state():
     st.session_state.setdefault('generated', ["Hello! I am here to provide answers to questions fetched from Database."])
     st.session_state.setdefault('past', ["Hello Buddy!"])
  
-def display_chat(chain):
+def display_chat():
     reply_container = st.container()
     container = st.container()
  
@@ -98,34 +21,39 @@ def display_chat(chain):
         with st.form(key='chat_form', clear_on_submit=True):
             user_input = st.text_input("Question:", placeholder="Ask me questions from MMS3", key='input')
             submit_button = st.form_submit_button(label='Send ⬆️')
+    #     st.markdown(utils3.convert_to_excel(response), unsafe_allow_html=True)
+    #     st.success('Downloaded the excel!!!')
        
         #Check if user submit question with user input and generate response of the question
         if submit_button and user_input:
-            generate_response(user_input,chain)
+            generate_response(user_input)
    
     #Display generated response to streamlit web UI
     display_generated_responses(reply_container)
  
-def generate_response(user_input,chain):
+def generate_response(user_input):
     with st.spinner('Spinning a snazzy reply...'):
-        output = conversation_chat(user_input,chain)
+        output = conversation_chat(user_input)
  
     st.session_state['past'].append(user_input)
     st.session_state['generated'].append(output)
  
-def conversation_chat(user_input,chain):
-    try:
-        response = utils.search(user_input)
-        print(response)
-        final_response = chain.invoke(f"Based on the following information generate human readable response: (**Use only the below info to generate response): \
-                                      Question: {user_input}\
-                                      Answer: {response}")
-        return final_response
-    except Exception as e:
-        error_statement = f"Sorry there is no relevant information about your question \'{user_input}\'.\
-                            Try asking a differnt question or rephrase the question :("
-        return e
-   
+def conversation_chat(user_input):
+    response = utils3.get_response(user_input)
+    print(response)
+    
+    # if st.button('Copy📋', key='copy_button'):
+    #     pyperclip.copy(response)
+    #     st.success('Text copied successfully!!!')
+
+    st.markdown(utils3.convert_to_excel(response), unsafe_allow_html=True)
+ 
+    # if st.button('Download Excel⬇️'):
+    #     st.markdown(utils3.convert_to_excel(response), unsafe_allow_html=True)
+    #     st.success('Downloaded the excel!!!')
+    #     #( working.....)
+       
+    return response
  
 def display_generated_responses(reply_container):
     if st.session_state['generated']:
@@ -147,10 +75,8 @@ def main():
  
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
-    chain=utils.parser_output()
-    display_chat(chain)
+   
+    display_chat()
  
 if __name__ == "__main__":
     main()
-
